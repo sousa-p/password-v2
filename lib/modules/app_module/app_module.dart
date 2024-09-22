3dbb1b305dad669180693273c9cba12a5dd30cb7
sousa-p <p.soousa7@gmail.com>
2024-09-22 18:45:23 -0300

    :rocket: feat: estrutura principal

---
+++ lib/modules/app_module/app_module.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  @override
+  void binds(Injector i) {
+    super.binds(i);
+  }
+
+  @override
+  void routes(RouteManager r) {
+    super.routes(r);
+  }
+}
